00cf882a161dcadab379afc668384f2a866d2783
Bryan Rodrigues <bryan.rodrigues@braip.com>
2023-03-03 14:17:06 -0300

    clear code

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <TheHeader />
     <transition mode="out-in">
       <router-view />
     </transition>
@@ -8,19 +7,12 @@
 </template>
 
 <script>
-import TheHeader from "./components/TheHeader.vue";
-
 export default {
   name: "App",
-  components: {
-    TheHeader,
-  },
 };
 </script>
 
 <style>
-/* Color Base:#42d9ff*/
-
 * {
   margin: 0;
   padding: 0;
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
 import LoginView from "../views/LoginView.vue";
 
 Vue.use(VueRouter);
@@ -10,11 +9,6 @@ const routes = [
     path: "/",
     name: "login",
     component: LoginView,
-  },
-  {
-    path: "/home",
-    name: "home",
-    component: HomeView,
   },
 ];
 
